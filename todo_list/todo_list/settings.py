e0ee3507141a1f51cbfcff7e19628e5c11627c3b
Beatriz Lemos <10408674+lemosbc@users.noreply.github.com>
2023-05-13 21:44:56 +0100

    Add local host to allowed hosts

--- todo_list/todo_list/settings.py
+++ todo_list/todo_list/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#jk)q+icyy-^ydm1&_&6107n(tt*na(yv$2@h%g&%-20%jb0s7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-env.eba-kyb4ivwz.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['django-env.eba-kyb4ivwz.us-west-2.elasticbeanstalk.com', '127.0.0.1']
 
 
 # Application definition
